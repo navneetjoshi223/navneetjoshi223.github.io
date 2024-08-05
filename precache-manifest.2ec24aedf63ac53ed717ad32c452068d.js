f2dfc72ab8ad64c85384cfe990e975b377fb15ae
navneetjoshi223 <joshi.nav@northeastern.edu>
2024-08-05 02:27:04 -0400

    Updates

---
+++ precache-manifest.2ec24aedf63ac53ed717ad32c452068d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de175fba8db5d9a422eaa272d3173d3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1e80da078cdbf4f5fbe",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "dfd075b0811419a44978",
+    "url": "/static/css/main.a97c1f42.chunk.css"
+  },
+  {
+    "revision": "b1e80da078cdbf4f5fbe",
+    "url": "/static/js/2.a8ed06c6.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.a8ed06c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd075b0811419a44978",
+    "url": "/static/js/main.110b6407.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
